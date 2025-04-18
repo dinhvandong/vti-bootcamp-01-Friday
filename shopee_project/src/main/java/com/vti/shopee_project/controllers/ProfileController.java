2074b591caf40f8451d0c61644814cbd0520ec95
dinhvandong <dongvan.ict3@gmail.com>
2025-04-18 17:03:46 +0700

    update

---
+++ shopee_project/src/main/java/com/vti/shopee_project/controllers/ProfileController.java
@@ -0,0 +1,35 @@
+package com.vti.shopee_project.controllers;
+
+import com.vti.shopee_project.entities.User;
+import com.vti.shopee_project.repositories.UserRepository;
+import com.vti.shopee_project.utils.JwtUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/profile")
+public class ProfileController {
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @Autowired
+    private UserRepository userRepo;
+
+    @GetMapping
+    public ResponseEntity<User> getProfile(@RequestHeader("Authorization") String authHeader) {
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            return ResponseEntity.status(401).build();
+        }
+
+        String token = authHeader.substring(7); // Remove "Bearer "
+        String email = jwtUtil.extractEmail(token);
+        User user = userRepo.findByEmail(email).orElseThrow(() -> new RuntimeException("Not found"));
+
+        return ResponseEntity.ok(user);
+    }
+}
